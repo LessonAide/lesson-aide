d00b9856aa7f025e7b61bf943b80f71794e62def
Habso Omane <habsoomane@Habsos-MBP.lan>
2025-05-04 18:19:31 -0400

    Added structure for store.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider } from "@mui/material/styles";
 import { RouterProvider } from "react-router-dom";
-import { theme } from '@/theme';
-import { routes } from '@/routes';
+import { Provider } from "react-redux";
+import { theme } from "@/theme";
+import { routes } from "@/routes";
+import store from "@/store";
+
 
 function App() {
-
   return (
-    <ThemeProvider theme={theme}>
-      <RouterProvider router={routes}/>
-    </ThemeProvider>
-  )
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={routes} />
+      </ThemeProvider>
+    </Provider>
+  );
 }
 
-export default App
+export default App;
