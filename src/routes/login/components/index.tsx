406163489b0568217b1fca397e67ee0fcc05b3c1
tolobayo <tolobayo1@gmail.com>
2025-05-13 18:41:52 -0400

    add background image and login components

--- src/routes/login/components/index.tsx
+++ src/routes/login/components/index.tsx
@@ -1,8 +1,51 @@
-import { useLogin } from '@/routes/login/hooks/useLogin';
+import { useLogin } from "@/routes/login/hooks/useLogin";
+import {
+  Container,
+  styled,
+  Typography,
+  TextField,
+  Box,
+  Switch,
+  Link,
+} from "@mui/material";
 
-export default function Login () {
+export default function Login() {
+  const bgImagePath = "/src/assets/images/login_background.png";
+  const BackgroundImage = styled(Container)(() => ({
+    backgroundImage: `url(${bgImagePath})`,
+    backgroundSize: "cover",
+    height: "100vh",
+  }));
+  const PageContainer = styled(Container)(() => ({ display: "flex" }));
+  const LoginContainer = styled(Container)(() => ({}));
+  const RememberContainer = styled(Box)(() => ({
+    display: "flex",
+    alignItems: "center",
+  }));
 
-    const { message } = useLogin();
-
-    return <div>{ message }</div>
+  return (
+    <PageContainer>
+      <BackgroundImage></BackgroundImage>
+      <LoginContainer>
+        <Typography variant="h5" align="center">
+          LessonAide
+        </Typography>
+        <TextField
+          id="login-username"
+          label="username"
+          variant="outlined"
+        ></TextField>
+        <TextField
+          id="login-username"
+          label="passowrd"
+          variant="outlined"
+        ></TextField>
+        <RememberContainer>
+          <Switch />
+          <Typography>Remember Me</Typography>
+        </RememberContainer>
+        <Link>Forgot Password?</Link>
+      </LoginContainer>
+    </PageContainer>
+  );
 }
